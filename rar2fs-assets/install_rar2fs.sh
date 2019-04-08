e2e0a2643ab11e093b68273efdfbd54511212ecf
Pretagonist <rapp.jerry@gmail.com>
2019-04-08 19:48:58 +0200

    Updated unrar source to latest supported
    Updated to 5.7.3. Latest as of now is 5.7.4 but I'm playing it safe.

--- rar2fs-assets/install_rar2fs.sh
+++ rar2fs-assets/install_rar2fs.sh
@@ -3,8 +3,8 @@
 cd /tmp
 git clone https://github.com/hasse69/rar2fs.git
 cd rar2fs
-wget http://www.rarlab.com/rar/unrarsrc-5.4.5.tar.gz
-tar -xvzf unrarsrc-5.4.5.tar.gz
+wget http://www.rarlab.com/rar/unrarsrc-5.7.3.tar.gz
+tar -xvzf unrarsrc-5.7.3.tar.gz
 cd unrar
 make lib
 make install-lib
